9cdaa7769dff17368494d9e35347afccfdfd61d9
Kenan Duman <kenanduman1988@gmail.com>
2023-04-20 12:08:59 +0200

    add simpson characters frontend

---
+++ resources/js/Pages/Simpsons.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Simpsons Characters</h1>
+        <ul>
+            <li v-for="simpson in simpsons" :key="simpson.id">
+                <h2>{{ simpson.name }}</h2>
+                <p>{{ simpson.quote }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: "Simpsons",
+    data() {
+        return {
+            simpsons: [],
+        }
+    },
+    async mounted() {
+        let characters = await axios.get('/api/characters')
+        this.simpsons = characters.data
+    },
+}
+</script>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from "@vitejs/plugin-vue"; //add this line
 
 export default defineConfig({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+        return pages[`./Pages/${name}.vue`]
+    },
     plugins: [
+        vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
